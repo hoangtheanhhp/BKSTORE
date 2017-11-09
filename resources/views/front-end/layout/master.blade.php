577b1d9ac09e67bb2dca7e75408080b3e283dfca
Minh Quang <quanghm@haposoft.com>
2017-11-09 17:58:49 +0700

    hehe

--- resources/views/front-end/layout/master.blade.php
+++ resources/views/front-end/layout/master.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
+  <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>@yield('pageTitle')</title>
@@ -12,6 +12,7 @@
   <link href='http://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
 
   <!-- Bootstrap -->
+  <!-- HEAD:resources/views/layout/master.blade.php -->
   <link rel="stylesheet" href="../css/bootstrap.min.css">
 
   <!-- Font Awesome -->
@@ -21,6 +22,7 @@
   <link rel="stylesheet" href="../ustora/css/owl.carousel.css">
   <link rel="stylesheet" href="../ustora/style.css">
   <link rel="stylesheet" href="../ustora/css/responsive.css">
+
   <link rel="stylesheet" href="/css/bootstrap.min.css">
 
   <!-- Font Awesome -->
@@ -30,41 +32,52 @@
   <link rel="stylesheet" href="/css/owl.carousel.css">
   <link rel="stylesheet" href="/css/style.css">
   <link rel="stylesheet" href="/css/responsive.css">
+  <!-- >>>>>>> 69da456c624063295269b99badef9a6e62179748:resources/views/front-end/layout/master.blade.php -->
 
   <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-
 </head>
 <body>
 
-		@include('front-end.layout.header')
-	<div class="rev-slider">
-		@yield('content')
-	</div> <!-- .container -->
-		@include('front-end.layout.footer')
+@include('front-end.layout.header')
+<div class="rev-slider">
+  @yield('content')
+</div> <!-- .container -->
+@include('front-end.layout.footer')
 
 
 
-	<!-- include js files -->
-  <!-- Latest jQuery form server -->
-  <script src="https://code.jquery.com/jquery.min.js"></script>
+<!-- include js files -->
+<!-- Latest jQuery form server -->
+<script src="https://code.jquery.com/jquery.min.js"></script>
 
-  <!-- Bootstrap JS form CDN -->
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<!-- Bootstrap JS form CDN -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 
-  <!-- jQuery sticky menu -->
-  <script src="/js/owl.carousel.min.js"></script>
-  <script src="/js/jquery.sticky.js"></script>
+<!-- jQuery sticky menu -->
+<script src="/js/owl.carousel.min.js"></script>
+<script src="/js/jquery.sticky.js"></script>
 
-  <!-- jQuery easing -->
-  <script src="/js/jquery.easing.1.3.min.js"></script>
+<!-- jQuery easing -->
+<script src="/js/jquery.easing.1.3.min.js"></script>
 
-  <!-- Main Script -->
-  <script src="/js/main.js"></script>
+<!-- Main Script -->
+<script src="/js/main.js"></script>
 
-  <!-- Slider -->
-  <script type="text/javascript" src={{ asset('js/bxslider.min.js') }}></script>
-  <script type="text/javascript" src="/js/script.slider.js"></script>
-
+<!-- Slider -->
+<script type="text/javascript" src={{ asset('js/bxslider.min.js') }}></script>
+<script type="text/javascript" src="/js/script.slider.js"></script>
+<script>
+    // $(document).ready(function($) {
+    // 	$(window).scroll(function(){
+    // 		if($(this).scrollTop()>150){
+    // 		$(".header-bottom").addClass('fixNav')
+    // 		}else{
+    // 			$(".header-bottom").removeClass('fixNav')
+    // 		}}
+    // 	)
+    //
+    // })
+</script>
 </body>
 </html>
