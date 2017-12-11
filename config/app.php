de49c89e0431884e2e732c6ce678b6ad207be859
Minh Quang <quanghm@haposoft.com>
2017-12-11 15:49:46 +0700

    abc

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://salty-lowlands-33631.herokuapp.com/'),
 
     /*
     |--------------------------------------------------------------------------
